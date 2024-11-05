71397c911452681c54a7b7dccbc444f73968a1c4
Lotris <163733100+Lotris-wow@users.noreply.github.com>
2024-11-06 01:48:28 +0200

    Add 29 level guide

--- 29_Level.lua
+++ 29_Level.lua
@@ -6,43 +6,61 @@ N Level 29 Required |N|You need to be at least level 29 to continue this guide|
 
 C Alien Egg |QID|4821| |N| Run directly south from the cave to the other side of the zone. Run around mountains when you see them. When you get to the other side, look for a hole in the wall containing an Alien Egg.|
 N If it isn’t there |N| If it isn’t there, run east until you see the next mountain pillar. Run north from here to another pillar mountain behind it. Look near the southwest corner of it for an egg.|
-N Once you have the egg |N| Once you have the egg, run west along the road to get back to Freewind Post.| 
-N Take the elevator up |N| Take the elevator up|
+
+R Freewind Post |N|(46.8, 47.3)| |Z|Thousand Needles|
 T Pacify the Centaur |QID|4841| 
-T Alien Egg |QID|4821| 
 A Grimtotem Spying |QID|5064| 
+T Alien Egg |QID|4821| 
 A Serpent Wild |QID|4865| 
 
-N Buy more consumables, repair, and sell |N|Buy more consumables, repair, and sell. 
+N Buy more consumables, repair, and sell |N|Buy more consumables, repair, and sell|
 N When you’re ready |N|When you’re ready, take the elevator down and run straight west past and around every bluff until you reach the wall of the zone|
 N Follow it until you come to Highperch |N|Follow it until you come to Highperch|
+R Highperch |N| (14.6, 32.8)| |Z|Thousand Needles|
+C Wind Rider |QID|4767| |N|Kill wyverns here and pick up eggs. Once you have 10, look for a pathway leading up to the southwest. Follow it all the way up to accept the quest| 
 
-C Wind Rider |QID|4767| |N|Kill wyverns here and pick up eggs. Once you have 10, look for a pathway leading up to the southwest. Follow it all the way up to accept the quest|
-A Homeward Bound |QID|4770| |N|Escort him down to safety|
-C Homeward Bound |QID|4770|
+A Homeward Bound |QID|4770| |N|Escort him down to safety (13.5, 40.3)| |Z|Thousand Needles|
+C Homeward Bound |QID|4770| 
+
 N Once you’re done |N|Once you’re done, run east from Highperch to find Whitereach Post|
-T Serpent Wild |QID|4865| 
+R Whitereach post
 T Homeward Bound |QID|4770|
-A Hypercapacitor Gizmo |QID|5151| |N|Hypercapacitor Gizmo
+T Serpent Wild |QID|4865| 
 A Sacred Fire |QID|5062| |N|Sacred Fire|
 
+A Hypercapacitor Gizmo |QID|5151| |N|Hypercapacitor Gizmo|
+C Hypercapacitor Gizmo |QID|5151| |N|Hypercapacitor Gizmo|
+T Hypercapacitor Gizmo |QID|5151| |N|Hypercapacitor Gizmo|
+N Leave the area by running east |N| Enter the lake you see there and find Incendia Agave in the water and around it. Collect 10 of them and run just south to find a patch leading up to Darkcloud Pinnacle|
+C Sacred Fire |QID|5062| |N|Sacred Fire|
 
-N |N|Leave the area by running east. Enter the lake you see there and find Incendia Agave in the water and around it. Collect 10 of them and run just south to find a patch leading up to Darkcloud Pinnacle|
+R Darkcloud Pinnacle |N|(30.8, 36.8)| |Z|Thousand Needles|
+R Run up |N|(34.2, 35.8)| |Z|Thousand Needles|
 
-N |N|Make your way up the bluff across multiple bridges until you reach a plateau with two routes to take. 
-N |N| Kill any Grimtotem tauren you see during the next few steps. Take the southern bridge for now.|
-N |N| Once you cross this bridge, run to the west to find another bridge leading to a smaller plateau. Cross the bridge and loot the note from the chest. 
-N |N| Go back across the bridge and run south to find a building. Enter it to find another note in a chest. Leave the building and go to the east of the bluff to find one more bridge.
-N |N| Cross this bridge and find the last chest with a note inside. Go back across the bridge and recross the northern bridge where you originally came from.
-N |N| This time take the eastern bridge to another plateau with two routes to take. Go across the northeast bridge. Turn left and run until you find Arnak Grimtotem.
-N |N| Kill him and pick up his hoof. Talk to the friendly tauren next to him to start the escort quest: Free at LastFree at Last. Walk with her and defend her as she exits the area. You’ll be ambushed multiple times. 
-N |N| Follow the road and run southeast until you are back at Freewind Post. Turn in Free at Last, Grimtotem Spying, WANTED: Arnak Grimtotem and Wind Rider.
-N |N| Ride the elevator back down and run west until you find some harpies at The Screeching Canyon. Kill them until you are level 30. You can also grind on the wyverns in Highperch
+N |N| Make your way up the bluff across multiple bridges until you reach a plateau with two routes to take. Kill any Grimtotem tauren you see during the next few steps. Take the southern bridge for now|
+N |N| Once you cross this bridge, run to the west to find another bridge leading to a smaller plateau. Cross the bridge and loot the note from the chest|
+N |N| Go back across the bridge and run south to find a building. Enter it to find another note in a chest. Leave the building and go to the east of the bluff to find one more bridge|
+N |N| Cross this bridge and find the last chest with a note inside. Go back across the bridge and recross the northern bridge where you originally came from|
 
+C Grimtotem Spying |Q|5064.2| |N|(33.7, 40)| |L||12766| |Z|Thousand Needles|
+C Grimtotem Spying |Q|5064.3| |N|(39.4, 41.6)| |L||12767| |Z|Thousand Needles|
+C Grimtotem Spying |Q|5064.1| |N|(31.8, 32.6)| |L||12765| |Z|Thousand Needles|
+
+N Go across the northeast bridge |N|This time take the eastern bridge to another plateau with two routes to take. Go across the northeast bridge. Turn left and run until you find Arnak Grimtotem (37.1, 31.9) (38.8, 28.6)| |L||12765| |Z|Thousand Needles|
+C Wanted - Arnak Grimtotem |QID|5147| |N| Kill him and pick up his hoof|
+N Talk to the friendly tauren next to him to start the escort quest
+A Free at Last |QID|4904| |N| Walk with her and defend her as she exits the area |N| Walk with her and defend her as she exits the area. You’ll be ambushed multiple times|
+N Follow the road and run southeast until you are back at Freewind Post
+C Free at Last |QID|4904| 
+R Freewind Post |N|(46.8, 47.3)| |Z|Thousand Needles|
+T Free at Last |QID|4904|
+T Grimtotem Spying |QID|5064|
+T WANTED: Arnak Grimtotem |QID|5147|
+T Wind Rider |QID|4767|
+R The Screeching Canyon |N| Ride the elevator back down and run west until you find some harpies at The Screeching Canyon. Kill them until you are level 30. You can also grind on the wyverns in Highperch|
 
 N Guide Complete |N|Tick to continue to the next guide|
 
 ]]
 end)
 
---[[
