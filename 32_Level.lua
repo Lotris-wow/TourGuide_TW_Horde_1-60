8ebb80fa5559a74a699c3ac136fec6709af7def1
ldmz <163733100+Lotris-wow@users.noreply.github.com>
2024-11-08 02:58:44 +0200

    Added 32

---
+++ 32_Level.lua
@@ -0,0 +1,54 @@
+TourGuide:RegisterGuide("32 Level", "33 Level", "Horde", function()
+
+return [[
+
+N Level 32 Required |N|You need to be at least level 32 to continue this guide|
+
+N Use the auction house, repair, and restock your consumables now
+
+F Freewind Post |N|Take a flight back to Freewind Post
+R Ride the elevator down and start following the road east
+R Ironstone Camp |N|(67.5, 63.2)| |Z|Thousand Needles|
+R The Shimmering Flats |N|Once you reach The Shimmering Flats, run southeast towards the center| |Z|Thousand Needles|
+R Mirage Raceway |N|Eventually you’ll find a racetrack area known as the Mirage Raceway (80, 77)||Z|Thousand Needles|
+
+A Wharfmaster Dizzywig |QID|1111|
+A Hemet Nesingwary |QID|5762|
+A Rocket Car Parts |QID|1110|
+A Hardened Shells |QID|1105|
+A Salt Flat Venom |QID|1104|
+A Load Lightening |QID|1176|
+A A Bump in the Road |QID|1175|
+
+
+N Run around the flats killing everything around your level that you see to finish the five quests you have here
+N Remember to loot your kills and pick up car parts when you see them
+
+C Hardened Shells |QID|1105|
+C Salt Flat Venom |QID|1104|
+C Load Lightening |QID|1176|
+C A Bump in the Road |QID|1175|
+C Rocket Car Parts |QID|1110|
+
+T Load Lightening |QID|1176|
+T A Bump in the Road |QID|1175|
+T Hardened Shells |QID|1105|
+T Salt Flat Venom |QID|1104|
+T Rocket Car Parts |QID|1110|
+
+N Accept two follow up quests
+A Goblin Sponsorship |QID|1178|
+A Martek the Exiled |QID|1106|
+N Kill turtles until you have 10 Turtle Meat
+N Turtle Meat Grind on enemies in the area until you are level 33
+
+
+N Guide Complete |N|Tick to continue to the next guide|
+
+]]
+end)
+
+--[[ 
+
+
+ ]]
