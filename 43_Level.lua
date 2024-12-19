71856324d2ab2e1347dd0c98a1bf8a390fc234b0
ldmz <163733100+Lotris-wow@users.noreply.github.com>
2024-12-19 23:42:29 +0200

    added 43

---
+++ 43_Level.lua
@@ -0,0 +1,154 @@
+TourGuide:RegisterGuide("43 Level", "44 Level", "Horde", function()
+
+return [[
+
+N Level 43 Required |N|You need to be at least level 43 to continue this guide|
+
+A This is Going to Be Hard (Part 3) |QID|778| |N|Kill the elemental that spawns and loot it|
+C This is Going to Be Hard (Part 3) |QID|778|
+T This is Going to Be Hard (Part 3) |QID|778|
+N Remember to equip the trinket |N|It will increase run speed by 40% for 10 seconds, but does have a 30 minute cooldown| |U|2820| |U|Nifty Stopwatch|
+
+R Kargath
+F Booty Bay
+
+T Sunken Treasure (Part 4) |QID|669| |N|Sunken Treasure (Part 4)|
+N Go to the inn and pick up these quests
+A Tran'rek |QID|2864| |N|Tran'rek|
+A Zanzil's Secret |QID|621| |N|Zanzil's Secret|
+A Venture Company Mining |QID|600| |N|Venture Company Mining|
+h Booty Bay |N|Make sure to set your hearthstone to Booty Bay while you're still here|
+N Leave the inn and go towards the bank to pick up the quest
+A Akiris by the Bundle |QID|617| |N|Akiris by the Bundle|
+
+N While you're here, check for any Green Hills of Stranglethorn pages |N|you are missing on the auction house|
+
+N Now go up a level on the docks to enter a house |N|and pick up the quests from “Sea Wolf” MacKinley|
+A Scaring Shaky |QID|606| |N|Scaring Shaky|
+A Stoley's Debt |QID|2872| |N|Stoley's Debt|
+A Intercepted Shipment |QID|40222|
+N It is on the docks, near the ship from Ratchet
+C Intercepted Shipment |QID|40222|
+T Intercepted Shipment |QID|40222|
+A Breaking Contracts |QID|40223| |Z|Arathi highlands|
+N Make your way to the northern exit of Booty Bay
+A The Bloodsail Buccaneers (Part 1) |QID|595| |N|The Bloodsail Buccaneers (Part 1) |
+
+N Go through the exit and head west 
+N Eventually you'll find some pirates and tents
+N Kill the pirates and use the |U|2083|Bloodsail Correspondence|
+T The Bloodsail Buccaneers (Part 1) |QID|595| |N|The Bloodsail Buccaneers (Part 1) |
+
+A The Bloodsail Buccaneers (Part 2) |QID|597| |N|The Bloodsail Buccaneers|
+N Leave the camp by running northwest across the bridge. Follow the coast until you reach the nagas
+N Kill naga here to complete Akiris by the Bundle. 
+
+C Akiris by the Bundle |QID|617| |N|Akiris by the Bundle|
+N Head southeast back along the coast and over the bridge
+
+N Run east towards the entrance of Booty Bay, but head north once you reach the road
+N When you see gorillas on the right, head over there and kill them. 
+N Stay here until Scaring Shaky is finished
+C Scaring Shaky |QID|606| |N|Scaring Shaky|
+
+N Head back towards the road and follow it south into Booty Bay. 
+T Scaring Shaky |QID|606| |N|Scaring Shaky|
+A Return to MacKinley |QID|607| |N|Return to MacKinley|
+N Make your way towards the inn
+T The Bloodsail Buccaneers (Part 2) |QID|597|
+A The Bloodsail Buccaneers (Part 3) |QID|599|
+
+N Head up the docks
+T Return to MacKinley |QID|607| |N|Return to MacKinley|
+A Voodoo Dues |QID|609| |N|Voodoo Dues|
+
+T The Bloodsail Buccaneers (Part 3) |QID|599|
+A The Bloodsail Buccaneers (Part 4) |QID|604|
+A Up to Snuff |QID|587| |N|Up to Snuff|
+
+N Head towards the exit again, but before leaving, go to the forge
+A Keep an Eye Out|QID|576| |N|Keep an Eye Out|
+
+N Now go out the exit to Booty Bay and head east this time
+N Kill any pirates you see and go to the northernmost pirate camp
+N Search for a clickable map and chart on the ground |N|If they aren't here, head south and check the boat. If not there, keep running south to reach a larger pirate camp. Pull this camp slowly. Look for the map and chart here|
+N Once you have the map and chart, kill all the pirates you can find |N|Once The Bloodsail Buccaneer, Up to Snuff, and Keep an Eye Out are finished, you can use your hearthstone to return to Booty Bay|
+C The Bloodsail Buccaneers (Part 4) |QID|604|
+C Up to Snuff |QID|587| |N|Up to Snuff|
+C Keep an Eye Out|QID|576| |N|Keep an Eye Out|
+
+H Booty Bay
+
+N Run upstairs and hand in T The Bloodsail Buccaneer  and Up to Snuff
+T The Bloodsail Buccaneers (Part 4) |QID|604|
+T Up to Snuff |QID|587| |N|Up to Snuff|
+
+A The Bloodsail Buccaneers (Part 5) |QID|608|
+N Make sure you have all your Green Hills of Stranglethorn pages now. Repair, buy consumables, and sell junk|
+
+N Head towards the exit of Booty Bay
+T Keep an Eye Out|QID|576| |N|Keep an Eye Out|
+N Run out of the gates
+N Follow the road north for quite a while north until you see the Ruins of Jubuwal on your right
+N From here, go northeast to find a path up with Venture Co. goons
+N Go along the path, killing the goblins to finish Venture Company Mining
+
+C Venture Company Mining |QID|600| |N|Venture Company Mining|
+N Return to the road and keep going west across it
+
+N After crossing the road, you should find raptors surrounding Gurubashi Arena
+K raptors |N|Kill them to collect 10| |L|3863.10| |L|Jungle Stalker Feathers|
+
+N Now run north, swimming across the water, to reach Grom'gol Base Camp
+R Grom'gol Base Camp
+T Mok'thardin's Enchantment (Part 2) |QID|572|
+A Mok'thardin's Enchantment (Part 3) |QID|571|
+N Follow the shore northwards to locate an  Elder Saltwater Crocolisk
+K Elder Saltwater Crocolisk |QID|| |N|Kill and loot it| |L|4105| |L|Elder Crocolisk Skin|
+N Go northwest now until you see a back way into the troll ruins
+N Follow it to find Nezzliok the Dire Gan'zulah Kill and loot them both to get their heads
+C |QID||
+N Leave the ruins and go east towards Nesingwary's Expedition Camp. 
+T Raptor Mastery
+A The Green Hills of Stranglethorn |QID|338| |N|The Green Hills of Stranglethorn|
+N Accept all four of the following quests
+A Chapter I |QID|339| |N|Chapter I|
+A Chapter II |QID|340| |N|Chapter II|
+A Chapter III |QID|341| |N|Chapter III|
+A Chapter IV |QID|342| |N|Chapter IV|
+T Chapter I |QID|339| |N|Chapter I|
+T Chapter II |QID|340| |N|Chapter II|
+T Chapter III |QID|341| |N|Chapter III|
+T Chapter IV |QID|342| |N|Chapter IV|
+T The Green Hills of Stranglethorn |QID|338|
+
+N Now go south back to Grom'gol Base Camp
+N Find the cauldron next to Nimboya and click it 
+T Bloodscalp Clan Heads |QID|584| |N|Bloodscalp Clan Heads| 
+A Speaking with Nezzliok |QID|585| |N|Speaking with Nezzliok|
+A Speaking with Gan'zulah |QID|586| |N|Speaking with Gan'zulah|
+N Sell junk and repair before leaving town
+
+N Head southeast and cross the river
+N Keep going until you find ruins with trolls Kill all of the trolls you see
+N Head to the camp and click on the skull to pick up the Ziata'jai Trophy |L|3907|
+N From here, there is a path leading to the east it
+N Go along it to access Balia'mah Ruins
+N Fight your way to the camp in the center to get the Balia'mah Trophy from the skulls |L|3906|
+
+N Go southeast from here to get to the Ruins of Zul'Mamwe
+N Here you can find another set of skulls containing the Zul'Mamwe Trophy |L|3908|
+N Keep killing trolls in the ruins until Skullsplitter Tusks and Split Bone Necklaces are both finished
+C Skullsplitter Tusks |QID|209| |N|Skullsplitter Tusks|
+C Split Bone Necklace |QID|598| |N|Split Bone Necklace|
+N Continue grinding here until level 44
+N At this point, you're probably going to want to do a dungeon to pad out your xp and get closer to the next level before continuing
+N Otherwise, you'll find a number of the quests in the next section to be a bit overwhelming
+N There are a few options for which dungeon you can go to |N|but Razorfen Downs is probably the most give for your gold, especially if you're Horde, as getting there will be convenient
+N Almost all of the quests for RFD can be acquired either from Varimathras in Undercity, if you are Horde, or Archbishop Benedictus in Stormwind if you are Alliance
+N The rest are picked up from NPCs just outside RFD's entrance, or inside the dungeon itself
+
+N Guide Complete |N|Tick to continue to the next guide|
+
+]]
+end)
