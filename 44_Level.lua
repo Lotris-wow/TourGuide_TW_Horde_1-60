fd0cd0b115944386d702ae6d8ed1f7f2b67d9f49
ldmz <163733100+Lotris-wow@users.noreply.github.com>
2024-12-23 16:30:10 +0200

    Added 44

---
+++ 44_Level.lua
@@ -0,0 +1,42 @@
+TourGuide:RegisterGuide("44 Level", "45 Level", "Horde", function()
+
+return [[
+
+N Level 44 Required |N|You need to be at least level 44 to continue this guide|
+
+R Ruins of Zul’Mamwe |N|Now find the main path through the Ruins of Zul’Mamwe until it ends in a cave|
+N Enter the cave and kill "Ana’thek the Cruel". He has two bodyguards, so consider focusing him first, looting, and then running away|
+
+N Guide Complete |N|Tick to continue to the next guide|
+
+]]
+end)
+--[[ 
+
+
+N |N|Kill any remaining trolls that you need for Speaking with Gan’zulah. Then head directly northwest and across the river to get back to Grom’gol Base Camp|
+N |N|Hand in "Split Bone Necklace". Use the cauldron and turn in "Speaking with Gan’Zulah" and "Speaking with Nezzliok". Accept the next quest: |QID|588| "The Fate of Yenniku"|
+N |N|Immediately hand in "The Fate of Yenniku". Accept the follow up quest: |QID|589| "The Singing Crystals". Take a flight to Booty Bay|
+N |N|Turn in "Skullsplitter Tusks". Run outside and turn in "Excelsior". Head down to turn in Venture Company Mining|
+N |N|Exit Booty Bay via the north exit and head east to the Wild Shore. This time you need to swim out to the pirate ships in the water and kill the captain aboard all three of them|
+N |N|Pull slowly and fight your way through each ship to the captain. The hardest is "npc/2546""Fleet Master Firallon". Try to save long cooldowns and potions for him|
+N |N|Keep an eye out for "object/2554""Cortello’s Riddle" on the ground on the bottom of each ship too. This contains "item/4056""Cortello’s Riddle." Use it to begin the quest: |QID|624| "Cortello’s Riddle"
+N |N|Once you’ve cleared all three ship’s captains and picked up the riddle, head west back to the shore. Go northwest to get back to the road|
+N |N|Follow the road northeast and return to Mistvale Valley. Kill gorillas until you get a sinew|
+N |N|Jump back onto the road and go north to arrive at the Ruins of Jubuwal. Slowly kill everything around "npc/2536""Jon-Jon the Crow". Once you can fight him with just one extra enemy, do so and loot his corpse|
+N |N|Continue behind Jon-Jon to find "npc/2535""Maury ‘Club-Foot’ Wilkins". Pull and focus one of his adds until they die. Run away to reset him and eat to restore your health. Repeat this until you can fight him alone. Kill Maury and loot him|
+N |N|Return west back to the road. Follow it south and go east at the first opportunity. Continue along the path as it goes south to reach the Ruins of Aboraz. Fight through it to find "npc/2537""Chucky ‘Ten Thumbs’". Kill and loot Chucky|
+N |N|Stay in the area and kill trolls until Zanzil’s Secret is finished. Return the road to the west once finished|
+N |N|Follow the road to the north. Keep going past the Ruins of Jubuwal and then immediately head east. Follow the path to reach Crystalvein Mine|
+N |N|Kill the basilisks here to collect three "item/3911""Pulsing Blue Shards". Use your hearthstone to return to Booty Bay once finished|
+N |N|Hand in "Zanzil’s Secret". Run upstairs and turn in "The Bloodsail Buccaneer". Go outside to turn in "Voodoo Dues"|
+N |N|Get on a flight to Stonard. Take the western road out of Stonard until it splits. Follow the northern path as it curves back eastward|
+N |N|Keep going until you find a bridge. Check underneath it to find "object/2553""A Soggy Scroll". Click it to turn in "Cortello’s Riddle". Pick up the next quest: the next part of |QID|625| "Cortello’s Riddle"|
+N |N|Head southeast back to Stonard. Take a flight to Grom’gol Base Camp|
+N |N|Turn in "Mok’thardin’s Enchantment" and "The Singing Crystals"|
+N |N|If you are not level 45 yet, go kill the trolls in the Ruins of Jubuwal until you are|
+
+
+B Cask of Merlot |QID|116.2| |N|Buy Cask of Merlot from Roberto Pupellyverbos in Valley Of Heroes (59.9, 76.7)| |Z|Stormwind City| |L|1941|
+
+]]
